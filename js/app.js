5b943d69595bf2a8f0fd62061af8a11ee24145d4
Anton Filimonov <filimlive@gmail.com>
2023-07-19 15:21:04 +0300

    Структурировал код, исправил баг с появлением кнопку загрузки на странице поискового запроса

--- js/app.js
+++ js/app.js
@@ -1,24 +1,21 @@
 /* Подключение к API */
-const API_KEY = "f46a3ff7-645d-422b-938c-4ab850430aa8";
+/* Резервный ключ "c00a42bb-f3ca-4225-a80e-eb89e511037e" */ /* f46a3ff7-645d-422b-938c-4ab850430aa8 */
+const API_KEY = "c00a42bb-f3ca-4225-a80e-eb89e511037e";
 
-/* Запрос к API и получение фильмов из подборки */
-let currentPage = 1;
-const moviesPerPage = 35;
-const API_URL_POPULAR =
-	"https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=";
+/* URL для получения фильмов из подборки */
+const API_URL_POPULAR = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=";
 
-/* Добавление поиска по ключевым словам */
-const API_URL_SEARCH =
-	"https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
+/* URL для поиска фильмов по ключевым словам */
+const API_URL_SEARCH = "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
 
-/* Запрос к API и получение информации о фильме (для модального окна) */
-const API_URL_MOVIE_DETAILS =
-	"https://kinopoiskapiunofficial.tech/api/v2.2/films/";
+/* URL для получения информации о фильме (для модального окна) */
+const API_URL_MOVIE_DETAILS = "https://kinopoiskapiunofficial.tech/api/v2.2/films/";
 
 /* Создаем ассинхронную функцию для получения массива данных */
 getMovies(API_URL_POPULAR);
+let isSearchQuery = false;
+let currentPage = 1;
 
-/* Создаем ассинхронную функцию для получения массива данных */
 async function getMovies(url) {
 	const resp = await fetch(url, {
 		headers: {
@@ -60,29 +57,31 @@ function showMovies(data) {
 			const movieElem = document.createElement("div");
 			movieElem.classList.add("movie");
 			movieElem.innerHTML = `
-				<div class="movie_cover-inner">
-					<img
-						src="${movie.posterUrlPreview}"
-						alt="${movie.nameRu}"
-						class="movie_cover"
-					/>
-					<div class="movie_cover-hover"></div>
-				</div>
-				<div class="movie_info">
-					<div class="movie_title">${movie.nameRu}</div>
-					<div class="movie_category">${movie.genres.map((genre) => ` ${genre.genre}`)}</div>
-					${!isPercentage(movie.rating) && movie.rating !== null
+              <div class="movie_cover-inner">
+                <img
+                  src="${movie.posterUrlPreview}"
+                  alt="${movie.nameRu}"
+                  class="movie_cover"
+                />
+                <div class="movie_cover-hover"></div>
+              </div>
+              <div class="movie_info">
+                <div class="movie_title">${movie.nameRu}</div>
+                <div class="movie_category">${movie.genres.map((genre) => ` ${genre.genre}`)}</div>
+                ${!isPercentage(movie.rating) && movie.rating !== null
 					? `<div class="movie_average movie_average-${getClassByRate(movie.rating)}">${movie.rating}</div>`
 					: ""}
-				</div>`;
+              </div>`;
 			movieElem.addEventListener("click", () => openModal(movie.filmId));
 			moviesElem.appendChild(movieElem);
 		}
 	});
 	/* Показываем кнопку "Загрузить еще", если есть еще страницы с фильмами и убираем кнопку на странице с поисковым запросом */
-	if (data.pagesCount != 0) {
+	if (currentPage <= data.pagesCount && !isSearchQuery) {
 		const loadMoreBtn = document.querySelector(".load-more-btn");
 		loadMoreBtn.style.display = "block";
+	} else {
+		loadMoreBtn.style.display = "none";
 	}
 }
 
@@ -108,9 +107,9 @@ form.addEventListener("submit", (e) => {
 	/* Формирование поискового запроса и очистка формы после отправки запроса */
 	const apiSearchUrl = `${API_URL_SEARCH}${search.value}`;
 	if (search.value) {
+		isSearchQuery = true; // Устанавливаем переменную isSearchQuery в true, когда выполняется поисковый запрос, чтобы убрать кнопку "Загрузить еще"
 		getMovies(apiSearchUrl);
 		search.value = "";
-		loadMoreBtn.style.display = "none" //Убираем кнопку "Загрузить еще" на странице поискового запроса
 	}
 });
 
@@ -132,33 +131,33 @@ async function openModal(id) {
 	document.body.classList.add("stop-scrolling");
 
 	modalElem.innerHTML = `
-		<div class="modal_card">
-			<div class="modal_button-close">
-				<i class="fa-regular fa-circle-xmark"></i>
-			</div>
-			<div class="modal_movie-content">
-				<h2>
-					<span class="modal_movie-title">${respData.nameRu}</span>
-					<span class="modal_movie-release-year"> - ${respData.year}</span>
-				</h2>
-				<ul class="modal_movie-info">
-					<li class="modal_movie-genre">
-						Жанр - ${respData.genres.map((el) => `<span>${el.genre}</span>`)}
-					</li>
-					${respData.filmLength ? `<li class="modal_movie-runtime">Время - ${respData.filmLength} минут</li>` : ""}
-					${respData.ratingKinopoisk !== null ? `<li>КиноПоиск: ${respData.ratingKinopoisk}</li>` : ""}
-					${respData.ratingImdb !== null ? `<li>IMDb: ${respData.ratingImdb}</li>` : ""}
-					<li>
-						Сайт:
-						<a class="modal_movie-site" href="${respData.webUrl}">${respData.webUrl}</a>
-					</li>
-					${respData.description !== null ? `<li class="modal_movie-overview">Описание - ${respData.description}</li>` : ""}
-				</ul>
-			</div>
-			<div class="modal_movie-poster">
-				<img class="modal_movie-backdrop" src="${respData.posterUrl}" alt="" />
-			</div>
-		</div>`;
+				<div class="modal_card">
+					<div class="modal_button-close">
+						<i class="fa-regular fa-circle-xmark"></i>
+					</div>
+					<div class="modal_movie-content">
+						<h2>
+							<span class="modal_movie-title">${respData.nameRu}</span>
+							<span class="modal_movie-release-year"> - ${respData.year}</span>
+						</h2>
+						<ul class="modal_movie-info">
+							<li class="modal_movie-genre">
+								Жанр - ${respData.genres.map((el) => `<span>${el.genre}</span>`)}
+							</li>
+							${respData.filmLength ? `<li class="modal_movie-runtime">Время - ${respData.filmLength} минут</li>` : ""}
+							${respData.ratingKinopoisk !== null ? `<li>КиноПоиск: ${respData.ratingKinopoisk}</li>` : ""}
+							${respData.ratingImdb !== null ? `<li>IMDb: ${respData.ratingImdb}</li>` : ""}
+							<li>
+								Сайт:
+								<a class="modal_movie-site" href="${respData.webUrl}">${respData.webUrl}</a>
+							</li>
+							${respData.description !== null ? `<li class="modal_movie-overview">Описание - ${respData.description}</li>` : ""}
+						</ul>
+					</div>
+					<div class="modal_movie-poster">
+						<img class="modal_movie-backdrop" src="${respData.posterUrl}" alt="" />
+					</div>
+				</div>`;
 
 	/* Навешиваем обработчик событий при клике на кнопку "Закрыть" */
 	const btnClose = document.querySelector(".modal_button-close");
@@ -184,7 +183,3 @@ window.addEventListener("keydown", (e) => {
 		closeModal();
 	}
 });
-
-
-
-
